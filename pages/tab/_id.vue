7cb039e2c759ab109b57719c2166fd41dd760a66
Axel Conceicao <axel.conceicao@epitech.eu>
2020-06-12 17:00:37 +0200

    Modifications qui seront validées : 	modifié :         pages/tab/_id.vue

--- pages/tab/_id.vue
+++ pages/tab/_id.vue
@@ -12,16 +12,12 @@
           <v-card class="my-custom-card mb-3" v-for="card in column.cards" :key="card.id" :color="card.color">
             <v-layout column class="pa-4 ma-0">
               <h3>{{ card.title }}</h3>
-              <v-dialog light v-model="colorPickerCard" max-width="300">
-                <v-color-picker v-model="card.color" flat></v-color-picker>
-                <v-btn color="success" text @click="colorPickerCard = false">Valider</v-btn>
-              </v-dialog>
               <v-card-actions class="ma-0 pa-0">
                 <v-spacer />
-                <v-btn @click.stop="colorPickerCard = true" color="warning" fab small dark>
+                <!-- <v-btn @click="cardColorSwitcher()" color="brown" fab x-small dark>
                   <v-icon>mdi-invert-colors</v-icon>
-                </v-btn>
-                <v-btn @click="deleteCard(card.id)" color="#363636" fab small dark>
+                </v-btn> -->
+                <v-btn @click="deleteCard(card.id)" color="#561705" fab x-small dark>
                   <v-icon>mdi-delete</v-icon>
                 </v-btn>
               </v-card-actions>
@@ -29,7 +25,7 @@
           </v-card>
           <!-- /each cards -->
           <!-- create Card -->
-          <v-card class="my-card">
+          <v-card class="my-card" color="#272727">
             <v-layout column class="pb-5 px-5 my-3">
               <v-text-field v-model="column.newCardTitle" placeholder="Nom de la carte"></v-text-field>
               <v-btn color="#363636" @click="createCard(column.newCardTitle, column.id, '#272727', '#ffffff')">Créer une carte</v-btn>
@@ -39,17 +35,31 @@
           <!-- /create Card -->
           <v-card-actions class="ma-0 pa-0 py-4">
             <v-spacer />
-            <v-dialog light v-model="colorPickerColumn" max-width="300">
-              <v-color-picker v-model="column.color" flat></v-color-picker>
-              <v-btn color="success" text @click="colorPickerColumn = false">Valider</v-btn>
-            </v-dialog>
-            <v-btn @click.stop="colorPickerColumn = true" color="warning" fab small dark>
+            <v-btn @click="columnColorSwitcher()" color="warning" fab x-small dark>
               <v-icon>mdi-invert-colors</v-icon>
             </v-btn>
-            <v-btn @click="deleteColumn(column.id)" color="error" fab small dark>
+            <v-btn @click="deleteColumn(column.id)" color="error" fab x-small dark>
               <v-icon>mdi-delete</v-icon>
             </v-btn>
           </v-card-actions>
+
+          <div v-if="colorPickerColumn" max-width="300">
+            <v-layout column class="pa-0">
+              <v-color-picker
+                value="#fff"
+                hide-canvas
+                hide-inputs
+                hide-mode-switch
+                style="margin-left: -20px; width: 300px; border-radius: 0;"
+                dark
+                :swatches="swatches"
+                show-swatches
+                v-model="column.color"
+                flat
+              >
+              </v-color-picker>
+            </v-layout>
+          </div>
         </v-card>
       </div>
       <!-- /each column -->
@@ -77,11 +87,17 @@ export default {
   data() {
     return {
       error: false,
-      color: '',
       colorPickerColumn: false,
-      colorPickerCard: false,
+      // colorPickerCard: false,
       columnTitle: '',
       columns: [],
+      swatches: [
+        ['#FF0000', '#AA0000', '#550000'],
+        ['#FFFF00', '#AAAA00', '#555500'],
+        ['#00FF00', '#00AA00', '#005500'],
+        ['#00FFFF', '#00AAAA', '#005555'],
+        ['#0000FF', '#0000AA', '#000055'],
+      ],
     }
   },
   created() {
@@ -171,11 +187,31 @@ export default {
           this.error = error.message
         })
     },
+
+    columnColorSwitcher() {
+      if (this.colorPickerColumn) {
+        this.colorPickerColumn = false
+      } else {
+        this.colorPickerColumn = true
+      }
+    },
+
+    cardColorSwitcher() {
+      if (this.colorPickerCard) {
+        this.colorPickerCard = false
+      } else {
+        this.colorPickerCard = true
+      }
+    },
   },
 }
 </script>
 
 <style scoped>
+.v-color-picker__input input {
+  color: white !important;
+}
+
 .mycontainer {
   max-width: 1600px;
 }
